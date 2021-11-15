a6d467351543af5fa5fabbe070ba492c97527a61
Mateus-msj3 <mateussouzaads@outlook.com>
2021-11-15 11:17:43 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './tutorial-details/tutorial-details.component';
+import { TutorialListComponent } from './tutorial-list/tutorial-list.component';
+import { TutorialLidtComponent } from './components/tutorial-lidt/tutorial-lidt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialListComponent,
+    TutorialLidtComponent
   ],
   imports: [
     BrowserModule,
